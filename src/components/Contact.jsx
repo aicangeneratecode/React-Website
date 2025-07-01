825c85ef92dffa2729282877f60e407f4635deec
aicangeneratecode <sashamogool@gmail.com>
2025-07-01 18:08:28 +0200

    Your commit message

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -53,13 +53,16 @@ const Contact = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/send", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
+      const response = await fetch(
+        "https://portfolioreact-574z.onrender.com/send",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
 
       const data = await response.json();
 
