123b8ed7679d0ffe8c325ee9311a7908cdc72d62
aicangeneratecode <sashamogool@gmail.com>
2025-07-01 19:37:30 +0200

    Add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainPage from "./pages/MainPage";
-import ProjectsPage from "./pages/ProjectsPage";
+import MainPage from "./pages/MainPage.jsx";
+import ProjectsPage from "./pages/ProjectsPage.jsx";
 
 function App() {
   return (
